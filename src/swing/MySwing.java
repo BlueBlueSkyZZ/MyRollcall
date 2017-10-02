31f4871b8f200b5d86df981972be5c8925767277
BlueBlueSkyZZ <493813966@qq.com>
2017-10-02 12:10:41 +0800

    添加了显示隐藏提问者按钮

--- src/swing/MySwing.java
+++ src/swing/MySwing.java
@@ -1,10 +1,10 @@
 package swing;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -19,6 +19,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 import util.CsvUtil;
@@ -28,10 +29,11 @@ import entity.Student;
 
 public class MySwing extends JFrame implements ActionListener, ItemListener{
 
+	int count;//计数器
 	List<Student> students = new ArrayList<Student>();
 	public MySwing() {
 		super("一个小小的点名软件");
-		setBounds(200, 200, 300, 200);
+		setBounds(200, 200, 450, 350);
 		this.addWindowListener(new winEventHandle());//窗口监听
 	}
 
@@ -94,15 +96,15 @@ public class MySwing extends JFrame implements ActionListener, ItemListener{
 	            		//获取时间戳选择回答问题的人
 	            		long time = System.currentTimeMillis();
 	                	if(time % 2 == 0){
-	                		System.out.println("讲ppt：" + todayStus.get(0).getName());
+	                		System.out.println("讲    ppt：" + todayStus.get(0).getName());
 	                		ppt = "讲ppt:" + todayStus.get(0).getStu_id() + todayStus.get(0).getName() + "  " + "\n";
 	                		System.out.println("回答问题：" + todayStus.get(1).getName());
-	                		answer = "回答问题：" + todayStus.get(1).getStu_id() + todayStus.get(1).getName() + "  " + "\n";
+	                		answer = "回答问题：" + todayStus.get(1).getStu_id() + todayStus.get(1).getName() + "  ";
 	                	}else{
-	                		System.out.println("讲ppt：" + todayStus.get(1).getName());
+	                		System.out.println("讲   ppt：" + todayStus.get(1).getName());
 	                		ppt = "讲ppt:" + todayStus.get(1).getStu_id() + todayStus.get(1).getName() + "  " + "\n";
 	                		System.out.println("回答问题：" + todayStus.get(0).getName());
-	                		answer = "回答问题：" + todayStus.get(0).getStu_id() + todayStus.get(0).getName() + "  " + "\n";
+	                		answer = "回答问题：" + todayStus.get(0).getStu_id() + todayStus.get(0).getName() + "  ";
 	                	}
 	                	for(int i = 2; i < todayStus.size(); i++){
 	                		System.out.println("提问者：" + todayStus.get(i).getName());
@@ -127,13 +129,38 @@ public class MySwing extends JFrame implements ActionListener, ItemListener{
 	            	
 	            	//创建JDialog窗口对象
 	    			JDialog dialog = new JDialog();
-	    			dialog.setBounds(200, 200, 300, 200);
+	    			dialog.setBounds(200, 200, 450, 350);
 	    			
 	    			JPanel panel = new JPanel(); 
+	    			JLabel label=new JLabel("第" + selectedNum + "组");
 	    			JTextArea textarea = new JTextArea();
-	    			textarea.setText(ppt + answer + ask);
-	    			
+	    			textarea.setText(ppt + answer);
+	    			JTextArea textarea2 = new JTextArea(); 
+	    			textarea2.setText(ask);
+	    			textarea2.setVisible(false);
+	    			JButton button_in = new JButton("显示提问者");
+	    			count = 0;
+	    			button_in.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent arg0) {
+							count = count + 1;
+							if(count%2 == 1){
+								textarea2.setVisible(true);
+								button_in.setText("隐藏提问者");
+							}else{
+								textarea2.setVisible(false);
+								button_in.setText("显示提问者");
+							}
+							
+						}
+					});
+	    			if(ask.equals("")){
+	    				button_in.setVisible(false);
+	    			}
+	    			panel.add(label);
 	    			panel.add(textarea);
+	    			panel.add(button_in);
+	    			panel.add(textarea2);
 	    			
 	    			dialog.add(panel);
 	    			dialog.setVisible(true);
@@ -149,6 +176,7 @@ public class MySwing extends JFrame implements ActionListener, ItemListener{
 	}
 	
 	public static void main(String args[]) {
+		setUIFont();
 		MySwing myMenu = new MySwing();
 		
 		myMenu.init();
@@ -208,7 +236,7 @@ public class MySwing extends JFrame implements ActionListener, ItemListener{
 	public void createRCWindow(String textContent){
 		//创建JDialog窗口对象
 		JDialog dialog = new JDialog();
-		dialog.setBounds(200, 200, 300, 200);
+		dialog.setBounds(200, 200, 450, 350);
 		
 		JPanel panel = new JPanel(); 
 		JTextArea textarea = new JTextArea();
@@ -220,5 +248,24 @@ public class MySwing extends JFrame implements ActionListener, ItemListener{
 		dialog.setVisible(true);
 	}
 
+	/**
+	 * 设置swing字体
+	 */
+	public static void setUIFont()
+	{
+		Font f = new Font("宋体",Font.PLAIN,20);
+		String   names[]={ "Label", "CheckBox", "PopupMenu","MenuItem", "CheckBoxMenuItem",
+				"JRadioButtonMenuItem","ComboBox", "Button", "Tree", "ScrollPane",
+				"TabbedPane", "EditorPane", "TitledBorder", "Menu", "TextArea",
+				"OptionPane", "MenuBar", "ToolBar", "ToggleButton", "ToolTip",
+				"ProgressBar", "TableHeader", "Panel", "List", "ColorChooser",
+				"PasswordField","TextField", "Table", "Label", "Viewport",
+				"RadioButtonMenuItem","RadioButton", "DesktopPane", "InternalFrame"
+		}; 
+		for (String item : names) {
+			 UIManager.put(item+ ".font",f); 
+		}
+	}
+	
 	
 }
